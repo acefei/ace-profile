9a6126af1f0b6eb781f8697f0133ab08dfe192ba
acefei <acefeilxy@gmail.com>
2019-11-17 19:10:45 -0800

    Update setup-win.ps1

--- installer/setup-win.ps1
+++ installer/setup-win.ps1
@@ -1,8 +1,6 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 $Out_File = ".\win.apps "
-((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/acefei/ace-profile/master/config/win.apps")) | Out-File -FilePath $Out_File
+wget "https://raw.githubusercontent.com/acefei/ace-profile/master/config/win.apps" -O $Out_File
 choco install $Out_File -y
 rm $Out_File
-
-pause
