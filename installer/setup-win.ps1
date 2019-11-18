da9f2cbe6b2af89e6c34a4d3e3bfe3f8bf4409ec
acefei <acefeilxy@gmail.com>
2019-11-17 18:55:27 -0800

    Update setup-win.ps1

--- installer/setup-win.ps1
+++ installer/setup-win.ps1
@@ -1,10 +1,3 @@
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{   
-    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
-    Start-Process powershell -Verb runAs -ArgumentList $arguments
-    Break
-}
-
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 $Out_File = ".\win.apps "
