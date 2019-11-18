cc300fbc9fb27f40edb85914c62a7849e13d9d14
acefei <acefeilxy@gmail.com>
2019-11-17 18:54:14 -0800

    Update setup-win.ps1

--- installer/setup-win.ps1
+++ installer/setup-win.ps1
@@ -11,3 +11,5 @@ $Out_File = ".\win.apps "
 ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/acefei/ace-profile/master/config/win.apps")) | Out-File -FilePath $Out_File
 choco install $Out_File -y
 rm $Out_File
+
+pause
