1a7cc6844a4f02e56be6c1c05039318c8a98f15d
acefei <acefeilxy@gmail.com>
2019-11-17 19:19:56 -0800

    Update setup-win.ps1

--- installer/setup-win.ps1
+++ installer/setup-win.ps1
@@ -1,6 +1,6 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$Out_File = ".\win.apps "
-wget "https://raw.githubusercontent.com/acefei/ace-profile/master/config/win.apps" -O $Out_File
+$Out_File = ".\packages.config"
+wget "https://raw.githubusercontent.com/acefei/ace-profile/master/config/winapps.config" -O $Out_File
 cinst $Out_File -y
 rm $Out_File
